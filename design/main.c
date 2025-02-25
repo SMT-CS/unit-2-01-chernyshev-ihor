e1c7d2b003273c3c54d3c3714a59d63b2669df1f
chernyshev-ihor <ihor.chernyshev@stu.ocsb.ca>
2025-02-25 19:04:07 +0000

    Fixed C and Python program

--- design/main.c
+++ design/main.c
@@ -11,8 +11,9 @@ float diameter = 42;
 
 int main() {
     // this function calculates the area of a circle
-    printf("If a circle has a radius of");
-    printf("%f", diameter);
+    printf("If a circle has a radius of ");
+    printf("%.0f", diameter);
+    printf(" mm\n");
     printf("Area is %.5f mm².\n", (M_PI * diameter));
     printf("\nDone.\n");
     return 0;
